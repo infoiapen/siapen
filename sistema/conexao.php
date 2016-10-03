b344b2b788532f6d2c18d1f658f5e17580059b16
Klaus Nascimento <klausnascimento@gmail.com>
2016-10-03 15:20:27 -0300

    Add comentários dentro do bloco php com info do master

--- sistema/conexao.php
+++ sistema/conexao.php
@@ -1,3 +1,9 @@
 <?php
+ /*************************************************************************************************************  
+ * @author Klaus Medeiros Nascimento                                                                                   *  
+ * Data: 25/11/2015                                                                                           *  
+ * Descrição: Instruções SELECT são recebidas integralmente  *  
+ * via parâmetro.                                                                                             *  
+ *************************************************************************************************************/
 require( dirname( __FILE__ ) . '/config/conexao.conf' );
 ?>
