aa938438ffeea905c1c662c1b1c15512850509f1
IAPEN <klaus@iapen.ap.gov.br>
2016-10-12 23:10:10 -0300

    Delete conexao.php

--- sistema/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
- /*************************************************************************************************************  
- * @author Klaus Medeiros Nascimento                                                                                   *  
- * Data: 25/11/2015                                                                                           *  
- * Descrição: Instruções SELECT são recebidas integralmente  *  
- * via parâmetro.                                                                                             *  
- *************************************************************************************************************/
-require( dirname( __FILE__ ) . '/config/conexao.conf' );
-?>
